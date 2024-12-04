GNOME/gnome-shell

gdm: Add "Login Failed" notification
One step toward implementing Web Login is adding the ability to
show "Login Failed" as a notification banner.

This is because with Web Login, the auth prompt is occluded so can't
be used to show an error message itself.

This commit adds the necessary code to show the Login Failed
notification, but doesn't actually call that code yet.

It will be used later when Web Login support is added.

js/gdm/authPrompt.js
@@ -9,6 +9,7 @@ import St from 'gi://St';
 
 import * as Animation from '../ui/animation.js';
 import * as AuthList from './authList.js';
+import * as AuthNotification from '../gdm/authNotification.js';
 import * as Batch from './batch.js';
 import * as GdmUtil from './util.js';
 import * as Params from '../misc/params.js';
@@ -628,6 +629,27 @@ export const AuthPrompt = GObject.registerClass({
     setForegroundMechanism(mechanism) {
         this._userVerifier.setForegroundService(mechanism.serviceName);
         this._userVerifier.setForegroundMechanism(mechanism);
+    }
+
+    showLoginFailedNotification() {
+        const source = new AuthNotification.AuthNotificationSource();
+        Main.messageTray.add(source);
+
+        const gicon = new Gio.ThemedIcon({ name: 'dialog-password-symbolic' });
+        this._loginFailedNotification = new MessageTray.Notification(source,
+            _('Login Failed'),
+            _('Please try again'),
+            { gicon });
+        this._loginFailedNotification.setTransient(true);
+        source.showNotification(this._loginFailedNotification);
+    }
+
+    hideLoginFailedNotification() {
+        if (!this._loginFailedNotification)
+            return;
+
+        this._loginFailedNotification.destroy();
+        this._loginFailedNotification = null;
     }
 
     reset(params) {

Rays-Robotics/project-lumos-DE

js/gdm/authPrompt.js
@@ -1,4 +1,5 @@
 import Clutter from 'gi://Clutter';
+import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import Pango from 'gi://Pango';
@@ -9,6 +10,8 @@ import * as Animation from '../ui/animation.js';
 import * as AuthList from './authList.js';
 import * as Batch from './batch.js';
 import * as GdmUtil from './util.js';
+import * as Main from '../ui/main.js';
+import * as MessageTray from '../ui/messageTray.js';
 import * as Params from '../misc/params.js';
 import * as ShellEntry from '../ui/shellEntry.js';
 import * as UserWidget from '../ui/userWidget.js';
@@ -694,6 +697,29 @@ export const AuthPrompt = GObject.registerClass({
     setForegroundMechanism(mechanism) {
         this._userVerifier.setForegroundService(mechanism.serviceName);
         this._userVerifier.setForegroundMechanism(mechanism);
+    }
+
+    showLoginFailedNotification() {
+        const source = new MessageTray.getSystemSource();
+
+        this._loginFailedNotification = new MessageTray.Notification({
+            source,
+            title: _('Login Failed'),
+            body: _('Please try again'),
+            isTransient: true,
+        });
+
+        this._loginFailedNotification.iconName = 'dialog-password-symbolic';
+
+        source.addNotification(this._loginFailedNotification);
+    }
+
+    hideLoginFailedNotification() {
+        if (!this._loginFailedNotification)
+            return;
+
+        this._loginFailedNotification.destroy();
+        this._loginFailedNotification = null;
     }
 
     reset(params) {